64b22dc9707591af49306f9845cfc573e88db958
tmbh <artem555game1@gmail.com>
2023-05-20 09:03:37 +0300

    load

---
+++ prak2/zad1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("l="); 
+            int l=Convert.ToInt32(Console.ReadLine());
+            l = l / 100;
+            Console.WriteLine($"{l}m");
+        }
+    }
+}
---
+++ prak2/zad2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("vvedite 3 znachnoe chislo");
+            string str = Console.ReadLine();
+            char[] a = str.ToCharArray();
+            Console.WriteLine($"{a[2]}{a[0]}{a[1]}");
+        }
+    }
+}
